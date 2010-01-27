60d3434192f44abaa9bdf29e12d1c04109651c63
rodries <rodries@wiimc.org>
2010-01-27 18:39:31 +0000

    - synced with official mplayer svn

--- source/mplayer/version.h
+++ source/mplayer/version.h
@@ -1,3 +1,3 @@
-#define VERSION "SVN-r24-4.4.2"
-#define BUILD_DATE "26/01/10"
+#define VERSION "SVN-r31-4.4.2"
+#define BUILD_DATE "27/01/10"
 #define MP_TITLE "%s "VERSION" (C) 2000-2010 MPlayer Team\n"

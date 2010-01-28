558c2ee4033a0e346cd046f7a89cf240eb1c0058
rodries@wiimc.org <rodries@wiimc.org@96508ff0-b81e-11de-97ce-e783fd196cab>
2010-01-28 20:47:28 +0000

    - synced with official

--- trunk/libs/libntfs/source/mft.c
+++ trunk/libs/libntfs/source/mft.c
@@ -32,14 +32,14 @@
 #ifdef HAVE_STDIO_H
 #include <stdio.h>
 #endif
-#ifdef HAVE_LIMITS_H
-#include <limits.h>
-#endif
 #ifdef HAVE_ERRNO_H
 #include <errno.h>
 #endif
 #ifdef HAVE_STRING_H
 #include <string.h>
+#endif
+#ifdef HAVE_LIMITS_H
+#include <limits.h>
 #endif
 #include <time.h>
 

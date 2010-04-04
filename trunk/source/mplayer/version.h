f5ae964975d7747bb733e4c1f2cec87b86f75e67
rodries@wiimc.org <rodries@wiimc.org@96508ff0-b81e-11de-97ce-e783fd196cab>
2010-04-04 10:53:03 +0000

    -fixed linker error

--- trunk/source/mplayer/version.h
+++ trunk/source/mplayer/version.h
@@ -1,3 +1,3 @@
-#define VERSION "SVN-r114-4.4.3"
-#define BUILD_DATE "04/04/10"
+#define VERSION "SVN-r30992-4.4.3"
+#define BUILD_DATE "03/04/10"
 #define MP_TITLE "%s "VERSION" (C) 2000-2010 MPlayer Team\n"

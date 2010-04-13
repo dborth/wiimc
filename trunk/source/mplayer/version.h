6e3f3d39d3c547568da3413df4c978492d129714
rodries@wiimc.org <rodries@wiimc.org@96508ff0-b81e-11de-97ce-e783fd196cab>
2010-04-13 10:31:58 +0000

    - synced with official mplayer svn

--- trunk/source/mplayer/version.h
+++ trunk/source/mplayer/version.h
@@ -1,3 +1,3 @@
-#define VERSION "SVN-r134-4.4.3"
-#define BUILD_DATE "07/04/10"
+#define VERSION "SVN-r155-4.4.3"
+#define BUILD_DATE "13/04/10"
 #define MP_TITLE "%s "VERSION" (C) 2000-2010 MPlayer Team\n"

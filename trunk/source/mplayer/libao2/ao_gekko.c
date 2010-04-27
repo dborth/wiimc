67f120dcdf9eb8e4d0f0858d94ad5e2791e16aa6
rodries@wiimc.org <rodries@wiimc.org@96508ff0-b81e-11de-97ce-e783fd196cab>
2010-04-27 18:46:11 +0000

    more audio tests

--- trunk/source/mplayer/libao2/ao_gekko.c
+++ trunk/source/mplayer/libao2/ao_gekko.c
@@ -160,6 +160,7 @@ static int init(int rate, int channels, int format, int flags)
 
 static void reset(void)
 {
+	while(buffered > 0)usleep(100);
 	playing = false;
 	while (AUDIO_GetDMABytesLeft() > 0)
 		usleep(100); // wait for next DMA
@@ -201,44 +202,42 @@ static int get_space(void)
 #define SWAP(x) ((x >> 16) | (x << 16))
 #define SWAP_LEN (BUFFER_SIZE / 4)
 
-static void copy_swap_channels(u32 *destination, u32 *source, int len)
+static void copy_swap_channels(u32 *destination, u32 *source)
 {
-	int counter;
-	for (counter = 0; counter < len/4; counter++)
+	for (int counter; counter < SWAP_LEN; counter++)
 		destination[counter] = SWAP(source[counter]);
-	for (; counter < SWAP_LEN; counter++)
-		destination[counter] = 0x0;
 }
 
 static int play(void *data, int len, int flags)
 {
 	int result = 0;
-	int size;
+	int size=len;
+
 	u8 *source = (u8 *)data;
 
 	while ((len >0)	&& (get_space() >= BUFFER_SIZE))
 	{
-		if(len>=BUFFER_SIZE)size=BUFFER_SIZE;
-		else size = len;
-		copy_swap_channels((u32 *)buffers[buffer_fill], (u32 *)source, size);
+		if (flags & AOPLAY_FINAL_CHUNK) memset(buffers[buffer_fill], 0, BUFFER_SIZE);
+		copy_swap_channels((u32 *)buffers[buffer_fill], (u32 *)source);
 		DCStoreRangeNoSync(buffers[buffer_play], BUFFER_SIZE);
 
 		buffer_fill = (buffer_fill + 1) % BUFFER_COUNT;
 		
-		result += size;
-		source += size;
+		result += BUFFER_SIZE;
+		source += BUFFER_SIZE;
 		buffered += BUFFER_SIZE;
 		
-		len -= size;
+		len -= BUFFER_SIZE;
 	}
 
-	if (!playing && (buffered >= PREBUFFER))
+	//if (!playing && (buffered >= PREBUFFER))
+	if (!playing && (buffered > BUFFER_SIZE))
 	{
 		playing = true;
 		switch_buffers();
 		AUDIO_StartDMA();
 	}
-
+	if (flags & AOPLAY_FINAL_CHUNK)	return size;
 	return result;
 }
 

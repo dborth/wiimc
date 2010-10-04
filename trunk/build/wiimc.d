da76a12887e0ee6d5c2bea27d3a9cc712270c79d
rodries@wiimc.org <rodries@wiimc.org@96508ff0-b81e-11de-97ce-e783fd196cab>
2010-10-04 18:21:25 +0000

    New linker code bss in mem2, latest libogc is needed (ogc_crt0.S)

---
+++ trunk/build/wiimc.d
@@ -0,0 +1,292 @@
+wiimc.o: c:/devkitPro/soft/wiimc/source/wiimc.cpp \
+ c:/devkitPro/libogc/include/gccore.h \
+ c:/devkitPro/libogc/include/ogc/dsp.h \
+ c:/devkitPro/libogc/include/gctypes.h \
+ c:/devkitPro/libogc/include/ogc/aram.h \
+ c:/devkitPro/libogc/include/ogc/arqueue.h \
+ c:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ c:/devkitPro/libogc/include/ogc/aram.h \
+ c:/devkitPro/libogc/include/ogc/arqmgr.h \
+ c:/devkitPro/libogc/include/ogc/audio.h \
+ c:/devkitPro/libogc/include/ogc/cache.h \
+ c:/devkitPro/libogc/include/ogc/card.h \
+ c:/devkitPro/libogc/include/ogc/cast.h \
+ c:/devkitPro/libogc/include/ogc/color.h \
+ c:/devkitPro/libogc/include/ogc/consol.h \
+ c:/devkitPro/libogc/include/ogc/gx_struct.h \
+ c:/devkitPro/libogc/include/ogc/dvd.h \
+ c:/devkitPro/libogc/include/ogc/disc_io.h \
+ c:/devkitPro/libogc/include/ogc/exi.h \
+ c:/devkitPro/libogc/include/ogc/gu.h \
+ c:/devkitPro/libogc/include/ogc/gx.h \
+ c:/devkitPro/libogc/include/ogc/lwp.h \
+ c:/devkitPro/libogc/include/ogc/gu.h \
+ c:/devkitPro/libogc/include/ogc/si.h \
+ c:/devkitPro/libogc/include/ogc/gx_struct.h \
+ c:/devkitPro/libogc/include/ogc/irq.h \
+ c:/devkitPro/libogc/include/ogc/context.h \
+ c:/devkitPro/libogc/include/ogc/lwp.h \
+ c:/devkitPro/libogc/include/ogc/mutex.h \
+ c:/devkitPro/libogc/include/ogc/message.h \
+ c:/devkitPro/libogc/include/ogc/semaphore.h \
+ c:/devkitPro/libogc/include/ogc/pad.h \
+ c:/devkitPro/libogc/include/ogc/tpl.h \
+ c:/devkitPro/libogc/include/ogc/gx.h \
+ c:/devkitPro/libogc/include/ogc/system.h \
+ c:/devkitPro/libogc/include/gcutil.h \
+ c:/devkitPro/libogc/include/ogc/video.h \
+ c:/devkitPro/libogc/include/ogc/video_types.h \
+ c:/devkitPro/libogc/include/ogc/usbgecko.h \
+ c:/devkitPro/libogc/include/ogc/video_types.h \
+ c:/devkitPro/libogc/include/ogc/texconv.h \
+ c:/devkitPro/libogc/include/ogc/ipc.h \
+ c:/devkitPro/libogc/include/ogc/es.h \
+ c:/devkitPro/libogc/include/ogc/stm.h \
+ c:/devkitPro/libogc/include/ogc/ios.h \
+ c:/devkitPro/libogc/include/ogc/usb.h \
+ c:/devkitPro/libogc/include/ogc/isfs.h \
+ c:/devkitPro/libogc/include/ogc/conf.h \
+ c:/devkitPro/libogc/include/ogc/usbstorage.h \
+ c:/devkitPro/libogc/include/ogc/mutex.h \
+ c:/devkitPro/libogc/include/ogc/wiilaunch.h \
+ c:/devkitPro/libogc/include/ogcsys.h \
+ c:/devkitPro/libogc/include/wiiuse/wpad.h \
+ c:/devkitPro/libogc/include/wiiuse/wiiuse.h \
+ c:/devkitPro/libogc/include/bte/bte.h \
+ c:/devkitPro/libogc/include/bte/bd_addr.h \
+ c:/devkitPro/libogc/include/di/di.h \
+ c:/devkitPro/libogc/include/ogc/ipc.h \
+ c:/devkitPro/soft/wiimc/source/utils/FreeTypeGX.h \
+ c:/devkitPro/portlibs/ppc/include/ft2build.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftheader.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/freetype.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftconfig.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftoption.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftstdlib.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/fterrors.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftmoderr.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/fterrdef.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/fttypes.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftsystem.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftimage.h \
+ c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftbitmap.h \
+ c:/devkitPro/soft/wiimc/source/utils/gettext.h \
+ c:/devkitPro/soft/wiimc/source/utils/mem2_manager.h \
+ c:/devkitPro/libogc/include/ogc/system.h \
+ c:/devkitPro/soft/wiimc/source/video.h \
+ c:/devkitPro/soft/wiimc/source/networkop.h \
+ c:/devkitPro/soft/wiimc/source/menu.h \
+ c:/devkitPro/soft/wiimc/source/libwiigui/gui.h \
+ c:/devkitPro/libogc/include/ogc/lwp_watchdog.h \
+ c:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ c:/devkitPro/soft/wiimc/source/libwiigui/../utils/FreeTypeGX.h \
+ c:/devkitPro/soft/wiimc/source/libwiigui/../video.h \
+ c:/devkitPro/soft/wiimc/source/libwiigui/../filelist.h \
+ c:/devkitPro/soft/wiimc/source/libwiigui/../input.h \
+ c:/devkitPro/soft/wiimc/source/libwiigui/../menu.h \
+ c:/devkitPro/soft/wiimc/source/libwiigui/../utils/mem2_manager.h \
+ c:/devkitPro/soft/wiimc/source/input.h \
+ c:/devkitPro/soft/wiimc/source/filelist.h \
+ c:/devkitPro/soft/wiimc/source/fileop.h \
+ c:/devkitPro/soft/wiimc/source/filebrowser.h \
+ c:/devkitPro/soft/wiimc/source/musicplaylist.h \
+ c:/devkitPro/soft/wiimc/source/wiimc.h \
+ c:/devkitPro/libogc/include/network.h \
+ c:/devkitPro/soft/wiimc/source/settings.h \
+ c:/devkitPro/soft/wiimc/source/mplayer/input/input.h \
+ c:/devkitPro/soft/wiimc/source/mplayer/m_config.h \
+ c:/devkitPro/soft/wiimc/source/mplayer/osdep/gx_supp.h
+
+c:/devkitPro/libogc/include/gccore.h:
+
+c:/devkitPro/libogc/include/ogc/dsp.h:
+
+c:/devkitPro/libogc/include/gctypes.h:
+
+c:/devkitPro/libogc/include/ogc/aram.h:
+
+c:/devkitPro/libogc/include/ogc/arqueue.h:
+
+c:/devkitPro/libogc/include/ogc/lwp_queue.h:
+
+c:/devkitPro/libogc/include/ogc/aram.h:
+
+c:/devkitPro/libogc/include/ogc/arqmgr.h:
+
+c:/devkitPro/libogc/include/ogc/audio.h:
+
+c:/devkitPro/libogc/include/ogc/cache.h:
+
+c:/devkitPro/libogc/include/ogc/card.h:
+
+c:/devkitPro/libogc/include/ogc/cast.h:
+
+c:/devkitPro/libogc/include/ogc/color.h:
+
+c:/devkitPro/libogc/include/ogc/consol.h:
+
+c:/devkitPro/libogc/include/ogc/gx_struct.h:
+
+c:/devkitPro/libogc/include/ogc/dvd.h:
+
+c:/devkitPro/libogc/include/ogc/disc_io.h:
+
+c:/devkitPro/libogc/include/ogc/exi.h:
+
+c:/devkitPro/libogc/include/ogc/gu.h:
+
+c:/devkitPro/libogc/include/ogc/gx.h:
+
+c:/devkitPro/libogc/include/ogc/lwp.h:
+
+c:/devkitPro/libogc/include/ogc/gu.h:
+
+c:/devkitPro/libogc/include/ogc/si.h:
+
+c:/devkitPro/libogc/include/ogc/gx_struct.h:
+
+c:/devkitPro/libogc/include/ogc/irq.h:
+
+c:/devkitPro/libogc/include/ogc/context.h:
+
+c:/devkitPro/libogc/include/ogc/lwp.h:
+
+c:/devkitPro/libogc/include/ogc/mutex.h:
+
+c:/devkitPro/libogc/include/ogc/message.h:
+
+c:/devkitPro/libogc/include/ogc/semaphore.h:
+
+c:/devkitPro/libogc/include/ogc/pad.h:
+
+c:/devkitPro/libogc/include/ogc/tpl.h:
+
+c:/devkitPro/libogc/include/ogc/gx.h:
+
+c:/devkitPro/libogc/include/ogc/system.h:
+
+c:/devkitPro/libogc/include/gcutil.h:
+
+c:/devkitPro/libogc/include/ogc/video.h:
+
+c:/devkitPro/libogc/include/ogc/video_types.h:
+
+c:/devkitPro/libogc/include/ogc/usbgecko.h:
+
+c:/devkitPro/libogc/include/ogc/video_types.h:
+
+c:/devkitPro/libogc/include/ogc/texconv.h:
+
+c:/devkitPro/libogc/include/ogc/ipc.h:
+
+c:/devkitPro/libogc/include/ogc/es.h:
+
+c:/devkitPro/libogc/include/ogc/stm.h:
+
+c:/devkitPro/libogc/include/ogc/ios.h:
+
+c:/devkitPro/libogc/include/ogc/usb.h:
+
+c:/devkitPro/libogc/include/ogc/isfs.h:
+
+c:/devkitPro/libogc/include/ogc/conf.h:
+
+c:/devkitPro/libogc/include/ogc/usbstorage.h:
+
+c:/devkitPro/libogc/include/ogc/mutex.h:
+
+c:/devkitPro/libogc/include/ogc/wiilaunch.h:
+
+c:/devkitPro/libogc/include/ogcsys.h:
+
+c:/devkitPro/libogc/include/wiiuse/wpad.h:
+
+c:/devkitPro/libogc/include/wiiuse/wiiuse.h:
+
+c:/devkitPro/libogc/include/bte/bte.h:
+
+c:/devkitPro/libogc/include/bte/bd_addr.h:
+
+c:/devkitPro/libogc/include/di/di.h:
+
+c:/devkitPro/libogc/include/ogc/ipc.h:
+
+c:/devkitPro/soft/wiimc/source/utils/FreeTypeGX.h:
+
+c:/devkitPro/portlibs/ppc/include/ft2build.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftheader.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/freetype.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftconfig.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftoption.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/config/ftstdlib.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/fterrors.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftmoderr.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/fterrdef.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/fttypes.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftsystem.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftimage.h:
+
+c:/devkitPro/portlibs/ppc/include/freetype2/freetype/ftbitmap.h:
+
+c:/devkitPro/soft/wiimc/source/utils/gettext.h:
+
+c:/devkitPro/soft/wiimc/source/utils/mem2_manager.h:
+
+c:/devkitPro/libogc/include/ogc/system.h:
+
+c:/devkitPro/soft/wiimc/source/video.h:
+
+c:/devkitPro/soft/wiimc/source/networkop.h:
+
+c:/devkitPro/soft/wiimc/source/menu.h:
+
+c:/devkitPro/soft/wiimc/source/libwiigui/gui.h:
+
+c:/devkitPro/libogc/include/ogc/lwp_watchdog.h:
+
+c:/devkitPro/libogc/include/ogc/lwp_queue.h:
+
+c:/devkitPro/soft/wiimc/source/libwiigui/../utils/FreeTypeGX.h:
+
+c:/devkitPro/soft/wiimc/source/libwiigui/../video.h:
+
+c:/devkitPro/soft/wiimc/source/libwiigui/../filelist.h:
+
+c:/devkitPro/soft/wiimc/source/libwiigui/../input.h:
+
+c:/devkitPro/soft/wiimc/source/libwiigui/../menu.h:
+
+c:/devkitPro/soft/wiimc/source/libwiigui/../utils/mem2_manager.h:
+
+c:/devkitPro/soft/wiimc/source/input.h:
+
+c:/devkitPro/soft/wiimc/source/filelist.h:
+
+c:/devkitPro/soft/wiimc/source/fileop.h:
+
+c:/devkitPro/soft/wiimc/source/filebrowser.h:
+
+c:/devkitPro/soft/wiimc/source/musicplaylist.h:
+
+c:/devkitPro/soft/wiimc/source/wiimc.h:
+
+c:/devkitPro/libogc/include/network.h:
+
+c:/devkitPro/soft/wiimc/source/settings.h:
+
+c:/devkitPro/soft/wiimc/source/mplayer/input/input.h:
+
+c:/devkitPro/soft/wiimc/source/mplayer/m_config.h:
+
+c:/devkitPro/soft/wiimc/source/mplayer/osdep/gx_supp.h:

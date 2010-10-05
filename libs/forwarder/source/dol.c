55225cea9e204acbda9eba7ffd54d7f82a4dd8c3
tantric <tantric@wiimc.org>
2010-10-05 05:34:39 +0000

    fix forwarder to work with latest code

--- libs/forwarder/source/dol.c
+++ libs/forwarder/source/dol.c
@@ -30,26 +30,25 @@ u32 load_dol_image(void *dolstart, struct __argv *argv)
 		{
 			if ((!dolfile->text_size[i]) || (dolfile->text_start[i] < 0x100))
 				continue;
-			VIDEO_WaitVSync();
-			ICInvalidateRange((void *) dolfile->text_start[i],
-					dolfile->text_size[i]);
+
 			memmove((void *) dolfile->text_start[i], dolstart
-					+ dolfile->text_pos[i], dolfile->text_size[i]);
+				+ dolfile->text_pos[i], dolfile->text_size[i]);
+
+			DCFlushRange ((void *) dolfile->text_start[i], dolfile->text_size[i]);
+			ICInvalidateRange((void *) dolfile->text_start[i], dolfile->text_size[i]);
 		}
 
 		for (i = 0; i < 11; i++)
 		{
 			if ((!dolfile->data_size[i]) || (dolfile->data_start[i] < 0x100))
 				continue;
-			VIDEO_WaitVSync();
+
 			memmove((void *) dolfile->data_start[i], dolstart
 					+ dolfile->data_pos[i], dolfile->data_size[i]);
-			DCFlushRangeNoSync((void *) dolfile->data_start[i],
+
+			DCFlushRange((void *) dolfile->data_start[i],
 					dolfile->data_size[i]);
 		}
-
-		memset((void *) dolfile->bss_start, 0, dolfile->bss_size);
-		DCFlushRange((void *) dolfile->bss_start, dolfile->bss_size);
 
 		if (argv && argv->argvMagic == ARGV_MAGIC)
 		{

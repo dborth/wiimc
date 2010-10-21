90a6f79305c28ed4bc4e40dcb9b3c27235d1d0c9
rodries@wiimc.org <rodries@wiimc.org@96508ff0-b81e-11de-97ce-e783fd196cab>
2010-10-21 17:24:33 +0000

    little changes from mplayerce

---
+++ trunk/source/mplayer/ffmpeg/libavutil/ppc/bswap.h
@@ -0,0 +1,44 @@
+/*
+ * This file is part of MPlayer CE.
+ *
+ * MPlayer CE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * MPlayer CE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MPlayer CE; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVUTIL_PPC_BSWAP_H
+#define AVUTIL_PPC_BSWAP_H
+
+#include <stdint.h>
+#include "config.h"
+#include "libavutil/attributes.h"
+
+#ifdef GEKKO
+#define av_bswap16 av_bswap16
+static av_always_inline av_const uint16_t av_bswap16(uint16_t x)
+{
+	uint16_t t;
+	asm("sthbrx	%0,0,%1" : : "r"(x), "r"(&t) : "memory");
+	return t;
+}
+
+#define av_bswap32 av_bswap32
+static av_always_inline av_const uint32_t av_bswap32(uint32_t x)
+{
+	uint32_t t;
+	asm("stwbrx	%0,0,%1" : : "r"(x), "r"(&t) : "memory");
+	return t;
+}
+#endif
+
+#endif /* AVUTIL_PPC_BSWAP_H */

8a4ffcc50eb995606254eb71be38c0b6555cd9d9
tantric <tantric@wiimc.org>
2011-02-23 06:24:56 +0000

    we can't support 2048x1080. even if we could, it wouldn't play well.

--- source/video.h
+++ source/video.h
@@ -18,9 +18,8 @@ extern "C" {
 #define MAX_TEX_WIDTH 768
 #define MAX_TEX_HEIGHT 480
 
-#define MAX_WIDTH 2048
-#define MAX_HEIGHT 1080
-
+#define MAX_WIDTH 1280
+#define MAX_HEIGHT 720
 
 void InitVideo();
 void InitVideo2();

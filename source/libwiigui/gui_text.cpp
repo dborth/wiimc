bd2e6f809917da23c17df4131b110aa4d47a5bbf
tantric <tantric@wiimc.org>
2012-03-21 14:52:05 +0000

    optimization - avoid unnecessary allocations

--- source/libwiigui/gui_text.cpp
+++ source/libwiigui/gui_text.cpp
@@ -161,6 +161,9 @@ wchar_t* GuiText::ToWchar(const char* t)
 
 void GuiText::SetText(const char * t)
 {
+	if(t && origText && strcmp(t, origText) == 0)
+		return;
+	
 	if(origText)
 		gui_free(origText);
 	if(text)
